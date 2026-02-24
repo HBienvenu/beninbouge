518cd0a0e3c0322270b72016a366f083d91290f8
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-02-24 16:26:21 +0100

    add

--- api/lambda.php
+++ api/lambda.php
@@ -5,26 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Déterminer le chemin de base
-$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
-
-// Vérifier si l'application est en mode maintenance
-if (file_exists($maintenance = $basePath . '/storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Charger l'autoloader
+// Remplace les lignes de chemins par :
+$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
+
+// Register the Composer autoloader...
 require $basePath . '/vendor/autoload.php';
 
-// Bootstrap Laravel
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once $basePath . '/bootstrap/app.php';
 
-// Gérer la requête
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Request::capture()
-);
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
--- public/index.php
+++ public/index.php
@@ -10,14 +10,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-// Remplace les lignes de chemins par :
-$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
-
 // Register the Composer autoloader...
-require $basePath . '/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once $basePath . '/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
